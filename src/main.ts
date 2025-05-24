a08d25d6921b913d6b74b80b5302ec4388497c59
nrshagor <noorerabbishagor@gmail.com>
2025-05-24 11:18:26 +0600

    feat(app): enable CORS with specific configuration for frontend access

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Enable CORS with specific configuration
+  app.enableCors({
+    origin: ['http://localhost:3000'], // Frontend origin
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
